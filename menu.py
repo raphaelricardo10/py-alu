c18f2f0fb53ad7bdc2d5c4ffbca5474b2eec3ff3
victor.diniz <victor@connectisystem.com.br>
2022-05-05 15:10:57 -0300

    Added ula to menu to share information across all the options

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+from ula import ULA
+
+
 class MenuOption:
     def __init__(self, message=None, callback=None) -> None:
         self.message = message
@@ -5,19 +8,23 @@ class MenuOption:
 
 
 class Menu:
-    def __init__(self, *options) -> None:
+    def __init__(self, *options, loop=False, ula=None) -> None:
         self.options: list[MenuOption] = options
 
+        self.loop = loop
+        self.ula = ula if ula else ULA()
+
     def show(self):
-        counter = 1
-        for option in self.options:
-            print(f'{counter}- {option.message}')
-            counter += 1
+        while self.loop:
+            counter = 1
+            for option in self.options:
+                print(f'{counter}- {option.message}')
+                counter += 1
 
-        ans = int(input('Selecione uma opção'))
-        selectedCallback = self.options[ans - 1].callback
+            ans = int(input('Selecione uma opção: '))
+            selectedCallback = self.options[ans - 1].callback
 
-        if isinstance(selectedCallback, Menu):
-            return selectedCallback.show()
+            if isinstance(selectedCallback, Menu):
+                return selectedCallback.show()
 
-        return selectedCallback()
+            selectedCallback(self.ula)
