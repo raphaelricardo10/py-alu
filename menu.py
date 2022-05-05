1b471c733878ce2f18d084a9f80f9fe90158af5d
victor.diniz <victor@connectisystem.com.br>
2022-05-05 15:19:44 -0300

    Added some loop breakers to exit the program

--- menu.py
+++ menu.py
@@ -15,16 +15,24 @@ class Menu:
         self.ula = ula if ula else ULA()
 
     def show(self):
-        while self.loop:
+        while True:
             counter = 1
+            lastOption = len(self.options) + 1
             for option in self.options:
                 print(f'{counter}- {option.message}')
                 counter += 1
-
+            print(f"{lastOption}- Sair")
             ans = int(input('Selecione uma opção: '))
+
+            if ans == lastOption:
+                break
+
             selectedCallback = self.options[ans - 1].callback
 
             if isinstance(selectedCallback, Menu):
                 return selectedCallback.show()
 
             selectedCallback(self.ula)
+
+            if not self.loop:
+                break
